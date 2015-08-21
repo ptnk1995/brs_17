9552d6ca58edf2d536283c78902d1de2d844bc6c
hellovietnam93 <hellovietnam93@gmail.com>
2015-08-21 12:25:53 +0700

    Cronjob to caculate book rating

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,3 +3,7 @@ env :PATH, ENV["PATH"]
 every 1.month, at: "end of the month at 11:59pm" do
   rake "request:delete_request", environment: :development
 end
+
+every 12.hours do
+  rake "book:average_rating", environment: :development
+end
